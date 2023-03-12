bede572a35d6ed96ae92ae680a1274f4682faba5
daryllzj <daryllzj@gmail.com>
2023-03-12 15:43:29 +0800

    revised request mapping

--- src/main/java/paf/day22_workshop_self/controller/RSVPRestController.java
+++ src/main/java/paf/day22_workshop_self/controller/RSVPRestController.java
@@ -25,13 +25,13 @@ import paf.day22_workshop_self.model.RSVP;
 import paf.day22_workshop_self.service.RSVPService;
 
 @RestController
-@RequestMapping(path="/")
+@RequestMapping(path="/api/rsvp")
 public class RSVPRestController {
 
     @Autowired
     RSVPService rsvpService;
 
-    @GetMapping(path="/api/rsvp" , produces= MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path="/all" ,produces= MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<RSVP>> getAllCustomers() {
 
         List<RSVP> rsvps = new LinkedList<>();
@@ -49,7 +49,7 @@ public class RSVPRestController {
         
     }
 
-    @GetMapping(path="/api/rsvp/" , produces= MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path="/search" , produces= MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> searchByName(@RequestParam("q") String name) {
 
         List<RSVP> rsvps = new LinkedList<>();
@@ -122,64 +122,64 @@ public class RSVPRestController {
     }
 
 
-    @PutMapping(path = "/api/rsvp/{email}", produces= MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> updateRSVP(@PathVariable("email") String email, @RequestBody RSVP rsvpRequest) {
+    // @PutMapping(path = "/{email}", produces= MediaType.APPLICATION_JSON_VALUE)
+    // public ResponseEntity<?> updateRSVP(@PathVariable("email") String email, @RequestBody RSVP rsvpRequest) {
 
-        RSVP rsvp = rsvpService.findByEmail(email);
+    //     RSVP rsvp = rsvpService.findByEmail(email);
 
-        if (rsvp != null) {
-            rsvp.setFullName(rsvpRequest.getFullName());
-            rsvp.setPhone(rsvpRequest.getPhone());
-            rsvp.setConfirmationDate(rsvpRequest.getConfirmationDate());
-            rsvp.setComments(rsvpRequest.getComments());
+    //     if (rsvp != null) {
+    //         rsvp.setFullName(rsvpRequest.getFullName());
+    //         rsvp.setPhone(rsvpRequest.getPhone());
+    //         rsvp.setConfirmationDate(rsvpRequest.getConfirmationDate());
+    //         rsvp.setComments(rsvpRequest.getComments());
 
-            boolean updated = rsvpService.update(rsvp);
+    //         boolean updated = rsvpService.update(rsvp);
 
-            if (updated) {
-                return ResponseEntity.status(HttpStatus.OK).build();
-            } else {
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-            }
-        } else {
-            String errorMessage = "No RSVP found";
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
-        }
-
-    }
-
-    
-
-    // @PutMapping(path = "/api/rsvp/update", produces= MediaType.APPLICATION_JSON_VALUE)
-    // public ResponseEntity<?> updateRSVP(@RequestBody MultiValueMap<String, String> form) throws java.text.ParseException {
-
-    //         String fullName = form.getFirst("fullName");
-    //         String email = form.getFirst("email");
-    //         String phone = form.getFirst("phone");
-    //         String confirmationDateString = form.getFirst("confirmationDate");
-    //         Date confirmationDate = null;
-    //             if (confirmationDateString != null) {
-    //             try {
-    //                 SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-    //                 java.util.Date parsed = format.parse(confirmationDateString);
-    //                 confirmationDate = new java.sql.Date(parsed.getTime());
-    //             } catch (ParseException e) {
-    //                 // Handle the parse error
-    //             }
-    //             }
-    //         String comments = form.getFirst("comments");
-            
-    //         RSVP updatedRsvp = new RSVP(fullName, email, phone, confirmationDate, comments);
-    
-    //         Boolean created = rsvpService.update(updatedRsvp);
-    
-    //         if(created){
-    //             // return ResponseEntity.status(HttpStatus.CREATED).build();
-    //             return new ResponseEntity<>(created, HttpStatus.CREATED);
-    //         } else{
+    //         if (updated) {
+    //             return ResponseEntity.status(HttpStatus.OK).build();
+    //         } else {
     //             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     //         }
+    //     } else {
+    //         String errorMessage = "No RSVP found";
+    //         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+    //     }
 
     // }
+
+    
+
+    @PutMapping(path = "/{email}", produces= MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> updateRSVP(@PathVariable("email") String email, @RequestBody MultiValueMap<String, String> form) throws java.text.ParseException {
+
+            String fullName = form.getFirst("fullName");
+            // String email = form.getFirst("email");
+            String phone = form.getFirst("phone");
+            String confirmationDateString = form.getFirst("confirmationDate");
+            Date confirmationDate = null;
+                if (confirmationDateString != null) {
+                try {
+                    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+                    java.util.Date parsed = format.parse(confirmationDateString);
+                    confirmationDate = new java.sql.Date(parsed.getTime());
+                } catch (ParseException e) {
+                    // Handle the parse error
+                }
+                }
+            String comments = form.getFirst("comments");
+            
+            RSVP updatedRsvp = new RSVP(fullName, email, phone, confirmationDate, comments);
+    
+            Boolean created = rsvpService.update(updatedRsvp);
+    
+            if(created){
+                // return ResponseEntity.status(HttpStatus.CREATED).build();
+                return new ResponseEntity<>(created, HttpStatus.CREATED);
+            } else{
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            }
+
+    }
 
 
 
